c9603f7110b085b47429b9ee4ba49b241a16f14a
afeiship <1290657123@qq.com>
2020-03-15 23:17:55 +0800

    feat: intial

---
+++ README.md
@@ -0,0 +1,26 @@
+# nx-file-utils
+> File utils for nx.
+
+## installation
+```rb
+# from gem
+gem 'nx-file-utils'
+# from git
+gem 'nx-file-utils', git: 'git@github.com:afeiship/nx-file-utils.git'
+```
+
+## usage
+```rb
+Nx::FileUtils::hello
+
+# hello world
+```
+
+## build/publish
+```shell
+# build
+gem build nx-file-utils.gemspec
+
+# publish
+gem push nx-file-utils-0.1.0.gem
+```
---
+++ lib/nx/file-utils.rb
@@ -0,0 +1,25 @@
+require "pathname"
+
+module Nx
+  class FileUtils
+    class << self
+      def sanitize(in_name)
+        bad_chars = ["/", '\\', "?", "%", "*", "|", '"', "<", ">", ".", "-", "（", "(", "）", ")"]
+        bad_chars.each do |char|
+          in_name.gsub!(" ", "")
+          in_name.gsub!("：", ":")
+          in_name.gsub!("，", ",")
+          in_name.gsub!(char, "_")
+          yield(in_name) if block_given?
+        end
+      end
+
+      def ordinal(in_name, in_options = {})
+        default_options = { number: 0, width: 3, char: "0", separator: "_" }
+        options = default_options.merge(in_options)
+        prefix = options[:number].to_s.rjust(options[:width], options[:char])
+        prefix + options[:separator] + in_name
+      end
+    end
+  end
+end
